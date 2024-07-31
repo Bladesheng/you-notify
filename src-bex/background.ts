51a24980fd354cbd0c90cdbf352fe71a37193418
Bladesheng <keadr23@gmail.com>
2024-07-31 19:47:45 +0200

    formatting

--- src-bex/background.ts
+++ src-bex/background.ts
@@ -1,75 +1,75 @@
 import { bexBackground } from 'quasar/wrappers';
 
-function openExtension () {
-  chrome.tabs.create(
-    {
-      url: chrome.runtime.getURL('www/index.html')
-    },
-    (/* newTab */) => {
-      // Tab opened.
-    }
-  );
+function openExtension() {
+	chrome.tabs.create(
+		{
+			url: chrome.runtime.getURL('www/index.html'),
+		},
+		(/* newTab */) => {
+			// Tab opened.
+		}
+	);
 }
 
 chrome.runtime.onInstalled.addListener(openExtension);
 chrome.action.onClicked.addListener(openExtension);
 
 declare module '@quasar/app-vite' {
-  interface BexEventMap {
-    /* eslint-disable @typescript-eslint/no-explicit-any */
-    log: [{ message: string; data?: any[] }, never];
-    getTime: [never, number];
+	interface BexEventMap {
+		/* eslint-disable @typescript-eslint/no-explicit-any */
+		log: [{ message: string; data?: any[] }, never];
+		getTime: [never, number];
 
-    'storage.get': [{ key: string | null }, any];
-    'storage.set': [{ key: string; value: any }, any];
-    'storage.remove': [{ key: string }, any];
-    /* eslint-enable @typescript-eslint/no-explicit-any */
-  }
+		'storage.get': [{ key: string | null }, any];
+		'storage.set': [{ key: string; value: any }, any];
+		'storage.remove': [{ key: string }, any];
+		/* eslint-enable @typescript-eslint/no-explicit-any */
+	}
 }
 
 export default bexBackground((bridge /* , allActiveConnections */) => {
-  bridge.on('log', ({ data, respond }) => {
-    console.log(`[BEX] ${data.message}`, ...(data.data || []));
-    respond();
-  });
+	bridge.on('log', ({ data, respond }) => {
+		console.log(`[BEX] ${data.message}`, ...(data.data || []));
+		respond();
+	});
 
-  bridge.on('getTime', ({ respond }) => {
-    respond(Date.now());
-  });
+	bridge.on('getTime', ({ respond }) => {
+		respond(Date.now());
+	});
 
-  bridge.on('storage.get', ({ data, respond }) => {
-    const { key } = data;
-    if (key === null) {
-      chrome.storage.local.get(null, (items) => {
-        // Group the values up into an array to take advantage of the bridge's chunk splitting.
-        respond(Object.values(items));
-      });
-    } else {
-      chrome.storage.local.get([key], (items) => {
-        respond(items[key]);
-      });
-    }
-  });
-  // Usage:
-  // const { data } = await bridge.send('storage.get', { key: 'someKey' })
+	bridge.on('storage.get', ({ data, respond }) => {
+		const { key } = data;
+		if (key === null) {
+			chrome.storage.local.get(null, (items) => {
+				// Group the values up into an array to take advantage of the bridge's chunk splitting.
+				respond(Object.values(items));
+			});
+		} else {
+			chrome.storage.local.get([key], (items) => {
+				respond(items[key]);
+			});
+		}
+	});
+	// Usage:
+	// const { data } = await bridge.send('storage.get', { key: 'someKey' })
 
-  bridge.on('storage.set', ({ data, respond }) => {
-    chrome.storage.local.set({ [data.key]: data.value }, () => {
-      respond();
-    });
-  });
-  // Usage:
-  // await bridge.send('storage.set', { key: 'someKey', value: 'someValue' })
+	bridge.on('storage.set', ({ data, respond }) => {
+		chrome.storage.local.set({ [data.key]: data.value }, () => {
+			respond();
+		});
+	});
+	// Usage:
+	// await bridge.send('storage.set', { key: 'someKey', value: 'someValue' })
 
-  bridge.on('storage.remove', ({ data, respond }) => {
-    chrome.storage.local.remove(data.key, () => {
-      respond();
-    });
-  });
-  // Usage:
-  // await bridge.send('storage.remove', { key: 'someKey' })
+	bridge.on('storage.remove', ({ data, respond }) => {
+		chrome.storage.local.remove(data.key, () => {
+			respond();
+		});
+	});
+	// Usage:
+	// await bridge.send('storage.remove', { key: 'someKey' })
 
-  /*
+	/*
   // EXAMPLES
   // Listen to a message from the client
   bridge.on('test', d => {
--- src-bex/my-content-script.ts
+++ src-bex/my-content-script.ts
@@ -1,11 +1,11 @@
 // Hooks added here have a bridge allowing communication between the BEX Content Script and the Quasar Application.
 // More info: https://quasar.dev/quasar-cli/developing-browser-extensions/content-hooks
 
-import { bexContent } from 'quasar/wrappers'
+import { bexContent } from 'quasar/wrappers';
 
 export default bexContent((/* bridge */) => {
-  // Hook into the bridge to listen for events sent from the client BEX.
-  /*
+	// Hook into the bridge to listen for events sent from the client BEX.
+	/*
   bridge.on('some.event', event => {
     if (event.data.yourProp) {
       // Access a DOM element from here.
@@ -17,4 +17,4 @@ export default bexContent((/* bridge */) => {
     }
   })
   */
-})
+});
