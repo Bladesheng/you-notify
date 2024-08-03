9ce2868ea2b92cddfa7021b6a627560b8c8c4f47
Bladesheng <keadr23@gmail.com>
2024-08-03 13:07:10 +0200

    parse notification message, move initialization

--- src-bex/background.ts
+++ src-bex/background.ts
@@ -1,4 +1,6 @@
 import { bexBackground } from 'quasar/wrappers';
+import pako from 'pako';
+import { BexBridge } from '@quasar/app-vite';
 
 type INotification = {
 	$type: string;
@@ -6,20 +8,6 @@ type INotification = {
 	content: string;
 	metadata: string;
 };
-
-function openExtension() {
-	chrome.tabs.create(
-		{
-			url: chrome.runtime.getURL('www/index.html#/options'),
-		},
-		(/* newTab */) => {
-			// Tab opened.
-		}
-	);
-}
-
-chrome.runtime.onInstalled.addListener(openExtension);
-chrome.action.onClicked.addListener(openExtension);
 
 declare module '@quasar/app-vite' {
 	interface BexEventMap {
@@ -32,70 +20,34 @@ declare module '@quasar/app-vite' {
 	}
 }
 
-let isMounted = false;
+// so that we have access to the bridge outside the bexBackground callback
+// (we can't put everything in the callback, because it is run whenever we open new tab, etc.)
+let bridge: BexBridge;
 
-export default bexBackground((bridge, allActiveConnections) => {
-	if (!isMounted) {
-		isMounted = true;
+chrome.runtime.onInstalled.addListener(openExtensionOptions);
+chrome.action.onClicked.addListener(openExtensionOptions);
 
-		setInterval(async () => {
-			console.log('interval');
-			fetchNotifications();
-		}, 60_000);
+chrome.notifications.onClicked.addListener((notificationId) => {
+	// you can pass data through the notification id - see here https://github.com/Seldszar/Gumbo/blob/main/src/background/index.ts#L350
+	const [id, notificationType, data] = notificationId.split(':');
 
-		fetchNotifications();
+	switch (notificationType) {
+		case 'issue': {
+			chrome.storage.local.get(['YouTrackUrl'], async (items) => {
+				const { YouTrackUrl } = items;
 
-		function fetchNotifications() {
-			chrome.storage.local.get(['YouTrackApiToken', 'YouTrackUrl'], async (items) => {
-				const { YouTrackApiToken, YouTrackUrl } = items;
+				const issueUrl = `https://${YouTrackUrl}/issue/${data}`;
 
-				const bearer = `Bearer ${YouTrackApiToken}`;
-				// https://stackoverflow.com/questions/51596809/how-do-i-access-user-notifications-via-rest-in-youtrack
-				const url = `https://${YouTrackUrl}/api/users/notifications?fields=id,content,metadata`;
-
-				try {
-					const res = await fetch(url, {
-						headers: {
-							Authorization: bearer,
-						},
-					});
-
-					const notifications: INotification[] = await res.json();
-					console.log(notifications);
-
-					chrome.storage.session.get(['displayedNotifications'], (items) => {
-						// IDs of all notifications that have already been show to the user
-						const alreadyDisplayed: string[] = items.displayedNotifications ?? [];
-
-						for (const notification of notifications) {
-							if (alreadyDisplayed.includes(notification.id)) {
-								// this one was already displayed
-								continue;
-							}
-
-							// @TODO decode content
-
-							chrome.notifications.create({
-								title: 'title: hi',
-								message: `message: you got new notification: @TODO decoded content`,
-								type: 'basic',
-								iconUrl: chrome.runtime.getURL('www/icons/favicon-128x128.png'),
-							});
-
-							alreadyDisplayed.push(notification.id);
-						}
-
-						// so that the new notifications don't get shown again
-						chrome.storage.session.set({
-							displayedNotifications: [...alreadyDisplayed],
-						});
-					});
-				} catch (err) {
-					console.error(err);
-				}
+				await chrome.tabs.create({ url: issueUrl });
 			});
 		}
 	}
+});
+
+setupInterval();
+
+export default bexBackground((_bridge, allActiveConnections) => {
+	bridge = _bridge;
 
 	bridge.on('log', ({ data, respond }) => {
 		console.log(`[BEX] ${data.message}`, ...(data.data || []));
@@ -158,3 +110,110 @@ export default bexBackground((bridge, allActiveConnections) => {
   })
    */
 });
+
+function openExtensionOptions() {
+	chrome.tabs.create(
+		{
+			url: chrome.runtime.getURL('www/index.html#/options'),
+		},
+		(/* newTab */) => {
+			// Tab opened.
+		}
+	);
+}
+
+function setupInterval() {
+	fetchNotifications();
+
+	setInterval(() => {
+		fetchNotifications();
+	}, 60_000);
+}
+
+function fetchNotifications() {
+	chrome.storage.local.get(['YouTrackApiToken', 'YouTrackUrl'], async (items) => {
+		const { YouTrackApiToken, YouTrackUrl } = items;
+
+		const bearer = `Bearer ${YouTrackApiToken}`;
+		// https://stackoverflow.com/questions/51596809/how-do-i-access-user-notifications-via-rest-in-youtrack
+		const url = `https://${YouTrackUrl}/api/users/notifications?fields=id,content,metadata`;
+
+		try {
+			const res = await fetch(url, {
+				headers: {
+					Authorization: bearer,
+				},
+			});
+			const notifications: INotification[] = await res.json();
+			console.log(notifications);
+
+			chrome.storage.session.get(['displayedNotifications'], (items) => {
+				// IDs of all notifications that have already been show to the user
+				const alreadyDisplayed: string[] = items.displayedNotifications ?? [];
+
+				for (const notification of notifications) {
+					if (alreadyDisplayed.includes(notification.id)) {
+						// this one was already displayed
+						continue;
+					}
+
+					const message = decodeAndDecompress(notification.content);
+					const { formattedMessage, issueId } = parseMessage(message);
+
+					chrome.notifications.create(`${Date.now()}:issue:${issueId}`, {
+						type: 'basic',
+						contextMessage: 'YouNotify',
+						title: 'New YouTrack notification',
+						message: formattedMessage,
+						isClickable: true,
+						iconUrl: chrome.runtime.getURL('www/icons/favicon-128x128.png'),
+					});
+
+					// so that the new notifications don't get shown again
+					alreadyDisplayed.push(notification.id);
+				}
+
+				chrome.storage.session.set({
+					displayedNotifications: alreadyDisplayed,
+				});
+			});
+		} catch (err) {
+			console.error(err);
+		}
+	});
+}
+
+function decodeAndDecompress(encodedString: string): string {
+	const base64Decoded = atob(encodedString);
+
+	const binaryString = new Uint8Array(base64Decoded.split('').map((char) => char.charCodeAt(0)));
+
+	const decompressed = pako.ungzip(binaryString, { to: 'string' });
+
+	return decompressed;
+}
+
+function parseMessage(message: string) {
+	const lines: string[] = message
+		.split('--------------------')
+		// the last part just says why you received this notification - nobody cares about that
+		.slice(0, -1)
+		// remove all the html stuff
+		.map((part) => part.replaceAll('<p>', ''))
+		.map((part) => part.replaceAll('</p>', ''))
+		// split it into actual lines
+		.flatMap((part) => part.split('\n'))
+		// remove all whitespace characters
+		.map((part) => part.trim())
+		// first and last lines are always empty
+		.slice(1, -1);
+
+	const issueUrl = lines.splice(3, 1)[0];
+	const urlParts = issueUrl.split('/');
+	const issueId = urlParts[urlParts.length - 1];
+
+	return {
+		issueId,
+		formattedMessage: lines.join('\n'),
+	};
+}
